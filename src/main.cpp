c0c95da4563dae89f56fa2deec299ace40aa706d
BenK <benk.qkq@gmail.com>
2022-11-26 13:01:09 +0300

    Added variable size window and key

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,24 @@
 
 #include <iostream>
 
+int g_windowSizeX = 1024;
+int g_windowSizeY = 768;
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int heigth)
+{
+    g_windowSizeX = width;
+    g_windowSizeY = heigth;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+}
+
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
+{
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+    }
+}
+
 int main(void)
 {
      /* Initialize the library */
@@ -17,15 +35,19 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    GLFWwindow*  Pwindow = glfwCreateWindow(1024, 768, "battle-sity", nullptr, nullptr);
-    if (!Pwindow)
+    GLFWwindow*  pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "battle-sity", nullptr, nullptr);
+    if (!pWindow)
     {
+        std::cout << "glfwCreateWindow faled!" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
+    glfwSetKeyCallback(pWindow, glfwKeyCallback);
+
     /* Make the window's context current */
-    glfwMakeContextCurrent(Pwindow);
+    glfwMakeContextCurrent(pWindow);
 	
 	if (!gladLoadGL())
 	{
@@ -33,18 +55,19 @@ int main(void)
 		return -1;
 	}
 	
-	std::cout << "OpenGL" << GLVersion.major << "." << GLVersion.minor << std::endl;
-	
+    std::cout << "Rendered: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
 	glClearColor(0, 0, 1, 1);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(Pwindow))
+    while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(Pwindow);
+        glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
