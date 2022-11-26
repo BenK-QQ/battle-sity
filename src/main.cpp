934184a606c0035812a66e67d84e9fbac5d7e4b4
BenK <benk.qkq@gmail.com>
2022-11-26 12:09:25 +0300

    Added base code

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,27 @@
 
 int main(void)
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
+     /* Initialize the library */
+    if (!glfwInit()) 
+    {
+        std::cout << "glfwInit faled!" << std::endl;
         return -1;
+    }
+        
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    GLFWwindow*  Pwindow = glfwCreateWindow(1024, 768, "battle-sity", nullptr, nullptr);
+    if (!Pwindow)
     {
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(Pwindow);
 	
 	if (!gladLoadGL())
 	{
@@ -33,13 +38,13 @@ int main(void)
 	glClearColor(0, 0, 1, 1);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(Pwindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(Pwindow);
 
         /* Poll for and process events */
         glfwPollEvents();
